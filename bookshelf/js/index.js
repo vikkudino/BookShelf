c730354cebac181a6e5341fd35849f890861c265
Aditya Vashishtha <vashishthaaditya20@gmail.com>
2017-02-10 18:26:40 +0530

    Completed Menu work and layout fix

--- bookshelf/js/index.js
+++ bookshelf/js/index.js
@@ -6,15 +6,47 @@ function searchBarUp(e) {
 		MainBar.className = "search-div";
 	}
 }
+var isMenuOpened = false;
 function menuOpen(argument) {
 	var menuClass = this.className;
-	if(menuClass="menu-button") {		
-		x = document.querySelectorAll(".float-menu")[0].style;					
-		
+	if(menuClass=="menu-button") {		
+		if(!isMenuOpened) {
+			x = document.querySelectorAll(".float-menu")[0].style.display="block";
+			isMenuOpened=true;		
+			argument.stopPropagation();		
+			return 0;				
+		}			
+		else {
+			x = document.querySelectorAll(".float-menu")[0].style.display="none";					
+			isMenuOpened =false;
+			return 0;
+		}			
 	}
 }
+function menuSafe(argument) {
+	//alert("Float CLick");
+	argument.stopPropagation();
+}
+function menuClose(argument) {
+	var menuClass = this.className;		
+	//alert(menuClass);
+	if(isMenuOpened) {		
+		if(menuClass!="float-menu") {				
+			x = document.querySelectorAll(".float-menu")[0].style.display="none";
+			isMenuOpened=false;			
+			return 0;					
+		} 		
+	}	
+}
+
 var sBar = document.querySelectorAll(".main-search-div > input[type=text]")[0];
 sBar.addEventListener('keyup',searchBarUp,false);
 
 var menuButton = document.querySelectorAll(".menu-button")[0];
 menuButton.addEventListener('click',menuOpen,false);
+
+var menu = document.querySelectorAll(".float-menu")[0];
+menu.addEventListener('click',menuSafe,false);
+
+var body = document.getElementsByTagName("html")[0];
+body.addEventListener('click',menuClose,false);
